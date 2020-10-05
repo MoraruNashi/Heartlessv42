e6268a32a06c376281abae9559b73b17a7d35da2
MoraruNashi <67695732+MoraruNashi@users.noreply.github.com>
2020-10-05 13:12:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ client.on("message", message => {
     /////////////////////////
 
 
-    if (message.content.startsWith(prefix + 'ui') || message.contente.startsWith(prefix + 'userinfo')){
+    if (message.content.startsWith(prefix + 'ui') || message.content.startsWith(prefix + 'userinfo')){
         const user = message.mentions.users.first() ? message.mentions.users.first() : message.author;
 
 
